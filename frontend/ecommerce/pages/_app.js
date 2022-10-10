306876cce9e0cda76e30b06ae23a1a0c304953ac
NelsonWang <wangnelson2@gmail.com>
2022-10-09 21:44:38 -0400

    state | context :tada:

--- frontend/ecommerce/pages/_app.js
+++ frontend/ecommerce/pages/_app.js
@@ -1,14 +1,19 @@
 import '../styles/globals.css'
 
 import { Layout } from '../components';
+import { Toaster } from 'react-hot-toast'
+import { StateContext } from '../context/StateContext';
 
 import React from 'react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 

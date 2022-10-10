3247163adf8d765ea9424eb9ec8aac95f1ec92a8
NelsonWang <wangnelson2@gmail.com>
2022-10-09 20:11:42 -0400

    footerbanner :tada:

--- frontend/ecommerce/components/Product.jsx
+++ frontend/ecommerce/components/Product.jsx
@@ -11,6 +11,8 @@ function Product({ product: { image, name, slug, price } }) {
           <img src={urlFor(image && image[0])}
             width={250}
             height={250} />
+            <p className="product-name">{name}</p>
+            <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
--- frontend/ecommerce/pages/index.js
+++ frontend/ecommerce/pages/index.js
@@ -24,7 +24,7 @@ const Home = ({ products, bannerData }) => {
         </div>
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   )
 }

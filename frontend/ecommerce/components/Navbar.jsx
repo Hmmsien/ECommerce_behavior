84d5b5a7e92f6e591dd5a9f7f4b4071d02115c48
NelsonWang <wangnelson2@gmail.com>
2022-10-11 17:54:11 -0400

    Navbar
    asdas

--- frontend/ecommerce/components/Navbar.jsx
+++ frontend/ecommerce/components/Navbar.jsx
@@ -14,7 +14,8 @@ function Navbar() {
   return (
     <div className='navbar-container' >
       <p className='="logo' >
-        <Link href="/" >JSM Headphones</Link>
+        <Link href="/" >JSM Headphones </Link>
+        <Link  className="nav-link" href="/" >| Graphics</Link>
       </p>
       <button type="button" className='cart-icon' onClick={() => setShowCart(true)} >
         <AiOutlineShopping />

0165f8842d260291eb0019e7b8593e41f08bab67
Hmmsien <hmyyqxi@gmail.com>
2022-10-15 16:04:09 -0400

    .gitignore is now working

---
+++ frontend/ecommerce/components/Description.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Description = ({Description}) => {
+    return (
+        <div className='description-container'>
+                <img src="/images/profile.jpg" alt="ecommerce" className='profile-image' />
+                <div className="descri">
+                    <h3>This data was collected by Open CDP project. This file contains behavior data for 7 months (from October 2019 to April 2020) from a large multi-category online store. We only work on the first 10 million rows of the 2019-Nov csv file.. 
+                    </h3>
+                </div>
+        </div>
+    )
+  }
+
+export default Description

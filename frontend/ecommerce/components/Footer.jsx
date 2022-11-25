5b6ab5a7e90cf901067072ff85426c6efba407c2
NelsonWang <wangnelson2@gmail.com>
2022-11-24 22:32:20 -0500

    safe incoming data load

--- frontend/ecommerce/components/Footer.jsx
+++ frontend/ecommerce/components/Footer.jsx
@@ -23,9 +23,9 @@ const Footer = () => {
         <a target="_blank" href="https://www.figma.com/file/bMmUmMtKJt9aUwKWHKW5LH/Data-Science?node-id=0%3A1"> < AiFillCodeSandboxSquare /></a>
         
       </p>
-      <FormControl sx={{ m: 1, minWidth: 2000 }}>
+      <FormControl sx={{  minWidth: "100%" }}>
 
-      <InputLabel id="demo-simple-select-label">Change Recommendation</InputLabel>
+      <InputLabel id="demo-simple-select-label">Change Recommendaion Model</InputLabel>
       <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -33,13 +33,10 @@ const Footer = () => {
           label="Set AI Model"
           onChange={handleChange}
         >
-        <MenuItem value={1}>
-          <em>Change Recommendation Model</em>
-        </MenuItem>
 
-          <MenuItem value={10}>Model 1</MenuItem>
-          <MenuItem value={20}>Model 2</MenuItem>
-          <MenuItem value={30}>Model 3</MenuItem>
+          <MenuItem value={1}>Model 1</MenuItem>
+          <MenuItem value={2}>Model 2</MenuItem>
+          <MenuItem value={3}>Model 3</MenuItem>
         </Select>
       </FormControl>
     </div>

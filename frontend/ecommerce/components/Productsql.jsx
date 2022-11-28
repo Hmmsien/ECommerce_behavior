41c90ef20262c0187563419ea640b998c481bff9
NelsonWang <wangnelson2@gmail.com>
2022-11-27 23:22:01 -0500

    display of images

---
+++ frontend/ecommerce/components/Productsql.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { urlFor } from '../lib/client'
+
+function ProductSQL({ product: { img_src, product_name, slug, price } }) {
+  return (
+    <div>
+      <Link href={`/products/${slug}`}>
+        <div className="product-card" >
+          <img src={img_src}
+            width={250}
+            height={250} />
+          <p className="product-name">{product_name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default ProductSQL

6d460a7199c5d299728a21bc9f0227e037b741fd
NelsonWang <wangnelson2@gmail.com>
2022-12-04 14:36:12 -0500

    appereal shoes

--- frontend/ecommerce/pages/category/[slug].js
+++ frontend/ecommerce/pages/category/[slug].js
@@ -3,11 +3,27 @@ import React from 'react'
 import { useStateContext } from '../../context/StateContext';
 import { client_base, base } from '../../lib/client_fast'
 
-const CategoriesGallery = ({ products })  => {
+import { ProductSQL } from '../../components';
 
+const CategoriesGallery = ({ products, categoryBanner }) => {
 
+    React.useEffect(() => {
+        console.log("categoryBanner", categoryBanner);
+        console.log("products", products);
+    }, [])
     return (
-        <div>CategoriesGallery</div>
+        <>
+            <h1>{categoryBanner.category_code}</h1>
+
+            <div className='products-container' >
+                {
+                    products?.map((product) => <>
+                        <ProductSQL product={product.Product} />
+                    </>)
+                }
+            </div>
+
+        </>
     )
 }
 
@@ -19,7 +35,7 @@ export const getStaticPaths = async () => {
         params: {
             slug: res_category.slug
         }
-    }) )
+    }))
     return {
         paths,
         fallback: 'blocking'
@@ -28,13 +44,16 @@ export const getStaticPaths = async () => {
 
 
 export const getStaticProps = async ({ params: { slug } }) => {
-    const PRODUCTS_TO_FETCH = 20;
+    const PRODUCTS_TO_FETCH = 200;
 
 
     const res = await fetch(`${base}/product_category_from_slug/${slug}?limit=${PRODUCTS_TO_FETCH}`)
     const products = await res.json()
+
+    const categoryBanner = products[0]["Banner"] ? products[0]["Banner"] : undefined;
+
     return {
-        props: {products}
+        props: { products, categoryBanner }
     }
 
 }

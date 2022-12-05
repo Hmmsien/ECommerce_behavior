77f3b5095c666295ab9262c5451eae50c6346909
NelsonWang <wangnelson2@gmail.com>
2022-12-04 22:57:31 -0500

    client fast

--- frontend/ecommerce/lib/client_fast.js
+++ frontend/ecommerce/lib/client_fast.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 export const client_base = axios
 
-export const base = "https://cjmpbdbwic.us-east-1.awsapprunner.com"
-// export const base = "http://127.0.0.1:8000"
+// export const base = "https://cjmpbdbwic.us-east-1.awsapprunner.com"
+export const base = "http://127.0.0.1:8000"
 
 
 

fd1283a7c5704d5af9b39feb42a81938c2eb8deb
NelsonWang <wangnelson2@gmail.com>
2022-12-06 00:54:43 -0500

    Update app.py

--- streamlit-visuals/app.py
+++ streamlit-visuals/app.py
@@ -14,7 +14,9 @@ st.markdown(""" <style>
 .des {color: #FF9633; text-align:justify; font-size:18px ;}
 </style> """, unsafe_allow_html=True)
 
-#image = Image.open('img/profile.jpg')
+
+image_file = Path(__file__).parent.resolve() / 'img/profile.jpg'
+image = Image.open(image_file)
 
 with st.sidebar:
     st.subheader('About our data')
@@ -22,8 +24,8 @@ with st.sidebar:
 st.markdown('<p class="font">ECommerce Behavior</p>',unsafe_allow_html=True)
 header_image, header_description = st.columns((4, 1))
 
-# with header_image:
-#     st.image(image)
+with header_image:
+    st.image(image)
 
 with header_description:
     st.markdown('<p class="des">This data was collected by Open CDP project. This file contains behavior data for 7 months (from October 2019 to April 2020) from a large multi-category online store. We only work on the first 10 million rows of the 2019-Nov csv file.. </p>',unsafe_allow_html=True)
